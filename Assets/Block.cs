01374c07d5631759d9616345b4a7904cf26b4ac1
NatsNatsumi <yohanenokubi@gmail.com>
2025-04-02 00:31:53 +0900

    初回コミット

---
+++ Assets/Block.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{    
+    public Block[] blocks;
+    public GameObject gameOverUI;
+    public GameObject gameClearUI;
+    private bool isGameClear = false;
+    void Start()
+    {        
+
+    }
+    void Update()
+    {
+        if(isGameClear != true)
+        {
+            if(DestroyAllBlocks())
+            {
+                //ゲームクリア
+                Debug.Log("ゲームクリア");
+                gameClearUI.SetActive(true);
+                isGameClear = true;
+            }
+        }
+        
+    }
+
+    //ブロック全部消えたか調べる
+    private bool DestroyAllBlocks()
+    {
+        foreach( Block b in blocks)
+        {
+            if( b !=null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("ゲームオーバー");
+        gameOverUI.SetActive(true);
+    }
+
+    public void GameRetry()
+    {
+        SceneManager.LoadScene("game");
+    }
+
+}
